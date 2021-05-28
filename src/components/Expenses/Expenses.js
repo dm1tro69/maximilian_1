1921ac886b9b21acc63796447afd76eafa902f04
dm1tro69 <dimolg22@gmail.com>
2021-05-28 11:01:39 +0300

    finished

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,17 +1,22 @@
-import ExpenseItem from './ExpenseItem';
+import React, { useState } from 'react';
+
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
-import { useState } from 'react';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
+// import './Expenses.css';
 
-function Expenses(props) {
+const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
+
     const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
     };
+
     const filteredExpenses = props.items.filter((expense) => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
+
     return (
         <div>
             <Card className='expenses'>
@@ -19,9 +24,11 @@ function Expenses(props) {
                     selected={filteredYear}
                     onChangeFilter={filterChangeHandler}
                 />
+                <ExpensesChart expenses={filteredExpenses} />
                 <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
     );
-}
+};
+
 export default Expenses;
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -4,31 +4,21 @@ const ExpenseForm = ({ onSaveExpenseDate, onCancel }) => {
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
-    // const [userInput, setUserInput] = useState({
-    //     enteredTitle: '',
-    //     enteredAmount: '',
-    //     enteredDate: '',
-    // });
+
     const titleChangeHandler = (e) => {
         setEnteredTitle(e.target.value);
-        // setUserInput({ ...userInput, enteredTitle: e.target.value });
-        // setUserInput((prevState) => {
-        //     return { ...prevState, enteredTitle: e.target.value };
-        // });
     };
     const amountChangeHandler = (e) => {
         setEnteredAmount(e.target.value);
-        // setUserInput({ ...userInput, enteredAmount: e.target.value });
     };
     const dateChangeHandler = (e) => {
         setEnteredDate(e.target.value);
-        // setUserInput({ ...userInput, enteredDate: e.target.value });
     };
     const submitHandler = (e) => {
         e.preventDefault();
         const expenseData = {
             title: enteredTitle,
-            amount: enteredAmount,
+            amount: +enteredAmount,
             date: new Date(enteredDate),
         };
         onSaveExpenseDate(expenseData);
